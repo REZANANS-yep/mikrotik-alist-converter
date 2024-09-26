0505dd68078ec60aabed00df4aa644a35386632c
REZANANS <rezanans@yahoo.com>
2024-09-26 08:20:54 +0200

    Update RouterOS-convert-edge.py
    Grammar fixes

--- RouterOS-convert-edge.py
+++ RouterOS-convert-edge.py
@@ -7,21 +7,19 @@ def is_ipv6(ip):
     except ValueError:
         return False
 
-def get_mikrotik_command(ip_cdr, list_name):
-    """Return MikroTik list add command based on IP/CDR and list name."""
-    return f"add address={ip_cdr} list={list_name}"
+def get_mikrotik_command(ip_CIDR, list_name):
+    """Return MikroTik list add command based on IP/CIDR and list name."""
+    return f"add address={ip_CIDR} list={list_name}"
 
 def main():
-    # Ask user for the list name
     list_name = input("Enter the name of the MikroTik list: ")
 
-    # Ask user if they want to overwrite the existing content of output.txt
     overwrite = input("Do you want to overwrite the existing content of output.txt? (yes/no): ").strip().lower()
     if overwrite not in ['yes', 'no']:
         print("Invalid input. Please answer with 'yes' or 'no'.")
         return
 
-    # Read the IP/CDRs from input.txt
+    # input.txt
     with open('input.txt', 'r') as f:
         ips = [line.strip() for line in f]
 
@@ -34,22 +32,20 @@ def main():
     else:
         include_ipv6 = 'yes'
 
-    # Filter IPs based on user choice
     if include_ipv6 == 'no':
         ips = [ip for ip in ips if not is_ipv6(ip)]
 
-    # Convert each IP/CDR to MikroTik command
     commands = [get_mikrotik_command(ip, list_name) for ip in ips]
 
     # Determine the write mode based on user input
     write_mode = 'w' if overwrite == 'yes' else 'a'
 
-    # Write the commands to output.txt
+    # output.txt
     with open('output.txt', write_mode) as f:
         for command in commands:
             f.write(command + '\n')
 
-    print(f"Converted IPs/CDRs to MikroTik commands {'and overwrote' if overwrite == 'yes' else 'and appended to'} 'output.txt'.")
+    print(f"Converted IP/CIDR to MikroTik commands {'and overwrote' if overwrite == 'yes' else 'and appended to'} 'output.txt'.")
 
 if __name__ == '__main__':
     main()
